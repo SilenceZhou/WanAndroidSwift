5a9732c0ca95c2d93ee1b52145ae8ea3f64891a7
SilenceZhou <1063123603@qq.com>
2019-11-21 15:25:54 +0800

    project init

---
+++ WanAndroidSwift/WanAndroidSwift/Main/TabBarController.swift
@@ -0,0 +1,25 @@
+//
+//  TabBarController.swift
+//  WanAndroidSwift
+//
+//  Created by zhouyun on 2019/11/21.
+//  Copyright © 2019 zhouyun. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        let child = HomeViewController()
+    
+        
+        
+
+        
+    }
+}
---
+++ WanAndroidSwift/WanAndroidSwift/WanAndroidSwift-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Alamofire/Alamofire-Swift.h>
+#import <SnapKit/SnapKit-Swift.h>
+#import <RxSwift/RxSwift-Swift.h>
+#import <RxCocoa/RxCocoa-Swift.h>
+#import <KakaJSON/KakaJSON-Swift.h>
+#import <SwiftyJSON/SwiftyJSON-Swift.h>
+#import <Kingfisher/Kingfisher-Swift.h>
+#import <IQKeyboardManagerSwift/IQKeyboardManagerSwift-Swift.h>
+#import <KRProgressHUD/KRProgressHUD-Swift.h>
+#import <DeviceKit/DeviceKit-Swift.h>
+#import <SwiftEventBus/SwiftEventBus-Swift.h>
+#import <SPPermissions/SPPermissions-Swift.h>
+#import <SkyFloatingLabelTextField/SkyFloatingLabelTextField-Swift.h>
